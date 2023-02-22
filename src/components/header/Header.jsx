1059a094c3e2ff10ebc4fd2cf32f3006f9cadf1a
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-22 10:07:02 +0300

    header added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
    
       <Container>
         
-      <h1>NBA LEGENDS</h1>
       <Image src={logo} width="200px"></Image>
+      <h1 className="my-2 font-monospace display-4 fw-bold">NBA LEGENDS</h1>
       </Container>
       
   
