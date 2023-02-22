63a8bc4f970556ed025a22d464e123170c4a9029
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-22 10:23:08 +0300

    card container added

--- src/components/cardcontainer/CardContainer.jsx
+++ src/components/cardcontainer/CardContainer.jsx
@@ -5,11 +5,31 @@ import { data } from "../../helper/data";
 
 const CardContainer = () => {
   return (
-    
+    <>
+      <Form.Control
+        placeholder="Search Player..."
+        type="search"
+        className="w-50 m-auto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      
       <Container className="card-container rounded-4 my-4 p-3">
-
-         {data.map((player)=>console.log(player))}
+  <Row className="g-3 justify-content-center">
+          {data
+            .filter((player) =>
+              player.name.toLowerCase().includes(search.trim().toLowerCase())
+            )
+            .map((player, i) => {
+              return (
+                <Col md={6} lg={4} xl={3} key={i}>
+                  <PlayerCard {...player} />
+                </Col>
+              );
+            })}
+        </Row>
+         
       </Container>
+      </>
       
       
       );
